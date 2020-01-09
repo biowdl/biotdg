b467ae391f9de1655168032ccca4c55286db88b1
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-01-09 16:44:39 +0100

    remove python 3.5 from supported classifiers

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
