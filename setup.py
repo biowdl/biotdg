2f86a8373b5b531c59649c0e01c805633e961787
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-01-14 11:11:50 +0100

    add version flag

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     install_requires=[
         "biopython",
         "cyvcf2",
+        "setuptools"  # To get the version during runtime.
     ],
     entry_points={"console_scripts": [
         "biotdg = biotdg:main"]}
